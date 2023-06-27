cd674977c04f7a035e01b4ced166c314b8c185ce
jean-christophe Maigrot <jean-christophe.maigrot@steeple.fr>
2023-06-27 23:07:23 +0200

    Keymap to keep only the current window.

--- lua/remap.lua
+++ lua/remap.lua
@@ -6,6 +6,10 @@ vim.keymap.set('n', '<leader>e', vim.cmd.Explore)
 vim.keymap.set('n', '<leader>ve', vim.cmd.Vexplore)
 vim.keymap.set('n', '<leader>se', vim.cmd.Sexplore)
 
+-- Remove all windows except the current one.
+vim.keymap.set('n', '<leader>o', vim.cmd.only)
+
+-- Toggles search highlighting
 vim.keymap.set('n', '<leader>hl', function ()
   vim.cmd.set('hlsearch!')
 end)
