3c589a95411ce9e87ced9e4886bef93b4e1fef66
jean-christophe Maigrot <jean-christophe.maigrot@steeple.fr>
2023-08-05 11:03:56 +0200

    Better set of options, add comments for clarity.

--- lua/set.lua
+++ lua/set.lua
@@ -1,7 +1,18 @@
+-- Set columns with relative numbers, and current line shows line number.
+-- Sign comlumn is fixed to avoid flickering when appearing/disappearing.
 vim.opt.number = true
 vim.opt.relativenumber = true
-
-vim.opt.tabstop = 2
 vim.opt.signcolumn = 'yes'
 
+-- Tab/indentation settings
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+
+-- Faster updatetime
 vim.opt.updatetime = 100
+
+-- Color line 80 as visual mark to avoid long lines.
+vim.opt.colorcolumn = '80'
