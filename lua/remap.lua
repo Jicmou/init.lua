429124c79bc3f6f9f88ad19007e62088db36f041
jean-christophe Maigrot <jean-christophe.maigrot@steeple.fr>
2023-10-29 18:18:45 +0100

    Add awesome sets and remaps.

--- lua/remap.lua
+++ lua/remap.lua
@@ -2,24 +2,50 @@
 vim.g.mapleader = " "
 
 -- Remaps for netrw mnemonically the same, just faster to type.
-vim.keymap.set('n', '<leader>e', vim.cmd.Explore)
-vim.keymap.set('n', '<leader>ve', vim.cmd.Vexplore)
-vim.keymap.set('n', '<leader>se', vim.cmd.Sexplore)
+vim.keymap.set("n", "<leader>e", vim.cmd.Explore)
+vim.keymap.set("n", "<leader>ve", vim.cmd.Vexplore)
+vim.keymap.set("n", "<leader>se", vim.cmd.Sexplore)
 
 -- Remove all windows except the current one.
-vim.keymap.set('n', '<leader>o', vim.cmd.only)
+vim.keymap.set("n", "<leader>o", vim.cmd.only)
 
--- Toggles search highlighting
-vim.keymap.set('n', '<leader>hl', function ()
-  vim.cmd.set('hlsearch!')
+-- Toggles search highlighting.
+vim.keymap.set("n", "<leader>hl", function()
+	vim.cmd.set("hlsearch!")
 end)
 
--- Remap window related moves
-vim.keymap.set('n', '<leader>hh', '<C-w>h')
-vim.keymap.set('n', '<leader>l', '<C-w>l')
-vim.keymap.set('n', '<leader>j', '<C-w>j')
-vim.keymap.set('n', '<leader>k', '<C-w>k')
-vim.keymap.set('n', '<leader>H', '<C-w>H')
-vim.keymap.set('n', '<leader>L', '<C-w>L')
-vim.keymap.set('n', '<leader>J', '<C-w>J')
-vim.keymap.set('n', '<leader>K', '<C-w>K')
+-- Remap window related moves.
+vim.keymap.set("n", "<leader>hh", "<C-w>h")
+vim.keymap.set("n", "<leader>l", "<C-w>l")
+vim.keymap.set("n", "<leader>j", "<C-w>j")
+vim.keymap.set("n", "<leader>k", "<C-w>k")
+vim.keymap.set("n", "<leader>H", "<C-w>H")
+vim.keymap.set("n", "<leader>L", "<C-w>L")
+vim.keymap.set("n", "<leader>J", "<C-w>J")
+vim.keymap.set("n", "<leader>K", "<C-w>K")
+
+-- Allow moving selected parts ud and down with J and K.
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<cr>gv=gv")
+
+-- Keep cursor at place when doing "J".
+vim.keymap.set("n", "J", "mzJ`z")
+
+-- Keep cursor in the middle when scrolling half-pages.
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- <leader>y to copy into the system clipboard.
+vim.keymap.set("n", "<leader>y", '"+y')
+vim.keymap.set("v", "<leader>y", '"+y')
+
+-- <leader>p to paste without losing the unnamed register
+vim.keymap.set("x", "<leader>p", '"_dP')
+
+-- <leader>d to delete into the black-hole register
+vim.keymap.set("n", "<leader>d", '"_d')
+vim.keymap.set("v", "<leader>d", '"_d')
+
+-- Remaps to navigate easily in the quickfix list
+vim.keymap.set("n", "C-j", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "C-k", "<cmd>cprevious<CR>zz")
--- lua/set.lua
+++ lua/set.lua
@@ -2,7 +2,7 @@
 -- Sign comlumn is fixed to avoid flickering when appearing/disappearing.
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.signcolumn = 'yes'
+vim.opt.signcolumn = "yes"
 
 -- Tab/indentation settings
 vim.opt.tabstop = 2
@@ -12,7 +12,10 @@ vim.opt.expandtab = true
 vim.opt.smartindent = true
 
 -- Faster updatetime
-vim.opt.updatetime = 100
+vim.opt.updatetime = 50
 
 -- Color line 80 as visual mark to avoid long lines.
-vim.opt.colorcolumn = '80'
+vim.opt.colorcolumn = "80"
+
+-- Keep 2 lines up and down when scrolling
+vim.opt.scrolloff = 2
