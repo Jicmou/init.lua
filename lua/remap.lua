8400bed08069a0da6a16016c24dda39feda4a986
jean-christophe Maigrot <jean-christophe.maigrot@steeple.fr>
2023-10-29 16:09:30 +0100

    Key bindings for window related commands.

--- lua/remap.lua
+++ lua/remap.lua
@@ -13,3 +13,13 @@ vim.keymap.set('n', '<leader>o', vim.cmd.only)
 vim.keymap.set('n', '<leader>hl', function ()
   vim.cmd.set('hlsearch!')
 end)
+
+-- Remap window related moves
+vim.keymap.set('n', '<leader>hh', '<C-w>h')
+vim.keymap.set('n', '<leader>l', '<C-w>l')
+vim.keymap.set('n', '<leader>j', '<C-w>j')
+vim.keymap.set('n', '<leader>k', '<C-w>k')
+vim.keymap.set('n', '<leader>H', '<C-w>H')
+vim.keymap.set('n', '<leader>L', '<C-w>L')
+vim.keymap.set('n', '<leader>J', '<C-w>J')
+vim.keymap.set('n', '<leader>K', '<C-w>K')
